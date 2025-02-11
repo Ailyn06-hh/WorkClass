ff1dee8507f1677bd67dbd70752e3b33b6343ef4
Ailyn06-hh <22151242@aguascalientes.tecnm.mx>
2025-02-11 17:13:20 -0600

    "Use of modifiers and composable functions 11/02/2025"

--- app/src/main/java/com/example/myapplicationenejun/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationenejun/MainActivity.kt
@@ -4,15 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplicationenejun.ui.theme.MyApplicationEneJunTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,46 +31,83 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
-            MyApplicationEneJunTheme{
+            MyApplicationEneJunTheme {
                 Column() {
-                    Row() {
-                        Text(text = "Hello World")
-                        Text(text = "Welcome to your first application")
+                    Column() {
+                        TextComposable()
+                        TextComposable()
+                        TextComposable()
                     }
                     Row() {
-                        Text(text = "Hello world")
-                        Text(text = "Welcome to your firs application")
+                        TextComposable()
+                        TextComposable()
+                        TextComposable()
                     }
-                    Row() {
-                        Text(text = "Hello world")
-                        Text(text = "Welcome to your firs application")
-                        }
-                        Text(text = "Hello world")
-                    Text(text = "Welcome to your firs application")
+                    Column {
+                        ModifierExample2()
+                    }
                 }
-                /* Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                     Greeting(
-                         name = "Android",
-                         modifier = Modifier.padding(innerPadding)
-                     )
-                 }*/
             }
         }
     }
 }
+@Preview(showBackground = true)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun TextComposable (name: String = "Empty") {
+        Text("Hello World")
+        Text(name)
+    }
+    @Preview(showBackground = true)
+    @Composable
+    fun ModifierExample1() {
+        Column(
+            modifier = Modifier
+                .padding(50.dp,30.dp, 20.dp, 10.dp)
+        ) {
+
+            Text("Hello World")
+        }
+    }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApplicationEneJunTheme {
-        Greeting("Android")
+fun ModifierExample2() {
+    Column(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxWidth()
+            .clickable(onClick = { clickAction() })
+    ) {
+
+        Text("Hello World")
     }
 }
+    fun clickAction(){
+        println("Column Clicked")
+    }
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample3() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(16.dp)
+            .background(Color.Yellow)
+            .border(width = 2.dp, color = Color.Green)
+            .width(200.dp),
+        horizontalAlignment = Alignment.CenterHorizontally ,
+        verticalArrangement = Arrangement.SpaceEvenly
+
+    ) {
+        TextComposable("1")
+        TextComposable("2")
+        TextComposable("3")
+        TextComposable("4")
+        TextComposable("5")
+        TextComposable("6")
+    }
+}
+
+
